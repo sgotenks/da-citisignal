97d1d7de4fd01b8779867eef0fbf2dcf1a8b1309
Markus Haack <mhaack@adobe.com>
2025-06-16 15:34:36 +0200

    fix: promo cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,12 +2,31 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
+
+  const promo = block.classList.contains('promo-cards');
+
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
+
+    if (promo) {
+      const tag = li.firstElementChild.querySelector('h5');
+      if (tag) {
+        li.lastElementChild.insertBefore(tag, li.lastElementChild.firstElementChild);
+      }
+      const picture = li.firstElementChild.querySelector('picture');
+      if (picture) {
+        picture.classList.add('cards-card-image');
+        li.lastElementChild.append(picture);
+      }
+      if (li.firstElementChild.textContent?.trim() === '') {
+        li.firstElementChild.remove();
+      }
+    }
+
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
--- scripts/aem.js
+++ scripts/aem.js
@@ -296,7 +296,7 @@ function createOptimizedPicture(
   eager = false,
   breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
 ) {
-  const url = new URL(src, window.location.href);
+  const url = !src.startsWith('http') ? new URL(src, window.location.href) : new URL(src);
   const picture = document.createElement('picture');
   const { pathname } = url;
   const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
